e5363160452db227199aac9d04c04765e3f78587
Aaron Harpole <aharpole@gmail.com>
2015-03-17 14:41:43 -0700

    remove some duplication from the Square code

--- square.rb
+++ square.rb
@@ -8,16 +8,8 @@ class Square
     @value = val
   end
   
-  def box_x
-    case x
-    when 1, 2, 3 then 1
-    when 4, 5, 6 then 2
-    when 7, 8, 9 then 3
-    end
-  end
-  
-  def box_y
-    case y
+  def box_for_coordinate(coordinate)
+    case coordinate
     when 1, 2, 3 then 1
     when 4, 5, 6 then 2
     when 7, 8, 9 then 3
@@ -25,6 +17,6 @@ class Square
   end
   
   def box
-    "#{box_x} #{box_y}"
+    "#{box_for_coordinate(x)} #{box_for_coordinate(y)}"
   end
 end
